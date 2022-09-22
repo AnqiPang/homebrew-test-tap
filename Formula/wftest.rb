e15a4e99c7011c000759cf2cef335d201a5abe2b
Anqi Pang <anqipans@amazon.com>
2022-09-22 16:16:18 -0700

    format

--- Formula/wftest.rb
+++ Formula/wftest.rb
@@ -1,5 +1,5 @@
 class Wftest < Formula
-  desc "test formula"
+  desc "Test formula"
   homepage "https://github.com/AnqiPang/homebrew-test-tap"
   url "https://github.com/AnqiPang/wftest/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "96c332d22b156d584e6bff4fe5d17138fc978eed505e84848309a5e1a7185246"
