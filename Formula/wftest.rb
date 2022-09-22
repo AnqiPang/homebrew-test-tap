a4eb031fcacf8e1aa1a799d733e2f668347cb2fb
Anqi Pang <anqipans@amazon.com>
2022-09-22 15:52:58 -0700

    wftest 0.0.1 (new formula)
    * v0.0.1
    * remove comments
    * format
    * update
    * update test
    * udpate
    
    Closes #1.
    
    Signed-off-by: Anqi Pang <55934700+AnqiPang@users.noreply.github.com>

---
+++ Formula/wftest.rb
@@ -0,0 +1,19 @@
+class Wftest < Formula
+  desc "Test formula"
+  homepage "https://github.com/AnqiPang/homebrew-test-tap"
+  url "https://github.com/AnqiPang/wftest/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "96c332d22b156d584e6bff4fe5d17138fc978eed505e84848309a5e1a7185246"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "greeter/main.go"
+    bin.install "main"
+    puts "test tap demo installed"
+  end
+
+  test do
+    assert_match "Hello, demo", shell_output("#{bin}/main")
+  end
+end
